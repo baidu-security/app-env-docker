71858b8c241f409eb8835e61707f05b6c51b0850
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-02-26 12:17:23 +0800

    Let us release that way

--- src/base/30-tomcat6.0/start.sh
+++ src/base/30-tomcat6.0/start.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
 export JAVA_HOME=/jdk/
+
+echo '[-] Starting Apache'
+apachectl -k start
+
+echo '[-] Starting Tomcat 6.0'
+bash /tomcat/bin/startup.sh
+
+echo '[-] Starting MariaDB'
+nohup mysqld_safe --datadir=/var/lib/mysql &>/dev/null &
+
+echo '[-] Waiting for MySQL to start ...'
+while true
+do
+	mysql -uroot -e 'select 1' &>/dev/null && break
+done
+
+echo '[-] Dropping shell'
+echo ' -  HostName:   ' $(hostname)
+echo ' -  IP address: ' $(ifconfig eth0 | awk '/inet / {print $2}')
+echo
+
+cd /tomcat/
+/bin/bash
+
