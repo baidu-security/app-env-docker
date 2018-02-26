e263837c42d120d814d55eb1823f5561b64531f0
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-02-26 13:22:15 +0800

    Let us release that way

--- src/base/31-tomcat7.0/start.sh
+++ src/base/31-tomcat7.0/start.sh
@@ -17,6 +17,9 @@ do
 	mysql -uroot -e 'select 1' &>/dev/null && break
 done
 
+echo '[-] Waiting for Tomcat to start ...'
+sleep 3 && curl -I 127.0.0.1:8080
+
 echo '[-] Dropping shell'
 echo ' -  HostName:   ' $(hostname)
 echo ' -  IP address: ' $(ifconfig eth0 | awk '/inet / {print $2}')
