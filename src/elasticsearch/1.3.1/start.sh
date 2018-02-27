2ceeef9288b32b32b8bcc567cb4cb1badb0bfc98
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-02-27 21:29:59 +0800

    Let us release that way

--- src/elasticsearch/1.3.1/start.sh
+++ src/elasticsearch/1.3.1/start.sh
@@ -5,14 +5,21 @@ export JAVA_HOME=/jdk/
 echo '[-] Starting Apache'
 apachectl -k start
 
+echo '[-] Starting ElasticSearch'
+bash /elasticsearch/bin/elasticsearch -d
+
+echo '[-] Waiting for ElasticSearch to start ...'
+while true
+do
+	curl -I 127.0.0.1:9200 &>/dev/null && break
+	sleep 1
+done
+
 echo '[-] Dropping shell'
 echo ' -  HostName:   ' $(hostname)
 echo ' -  IP address: ' $(ifconfig eth0 | awk '/inet / {print $2}')
 echo
 
-echo '[-] Starting ElasticSearch'
-bash /elasticsearch/bin/elasticsearch
-
-# cd /elasticsearch/
-# /bin/bash
+cd /elasticsearch/
+/bin/bash
 
