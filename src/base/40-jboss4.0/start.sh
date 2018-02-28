4eaebee6ea32a795e74b9fadc2d9ed305af32436
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-02-28 13:22:48 +0800

    Let us release that way

--- src/base/40-jboss4.0/start.sh
+++ src/base/40-jboss4.0/start.sh
@@ -14,14 +14,22 @@ do
 	mysql -uroot -e 'select 1' &>/dev/null && break
 done
 
+echo '[-] Starting JBoss 4.0'
+echo ' -  Logging to /var/log/jboss.log'
+nohup bash /jboss/bin/run.sh -b 0.0.0.0 &> /var/log/jboss.log &
+
+echo '[-] Waiting for JBoss to start ...'
+while true
+do
+	curl -I 127.0.0.1:8080 &>/dev/null && break
+	sleep 1
+done
+
 echo '[-] Dropping shell'
 echo ' -  HostName:   ' $(hostname)
 echo ' -  IP address: ' $(ifconfig eth0 | awk '/inet / {print $2}')
 echo
 
-echo '[-] Starting JBoss 4.0'
-bash /jboss/bin/run.sh -b 0.0.0.0
-
-# cd /jboss/
-# /bin/bash
+cd /jboss/
+/bin/bash
 
