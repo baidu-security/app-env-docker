ee37af38c0cb1304e8062582b23992c86eda4d63
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-03-06 00:17:45 +0800

    Let us release that way

--- src/solr/7.0.1/start.sh
+++ src/solr/7.0.1/start.sh
@@ -7,11 +7,11 @@ echo '[-] Starting ZooKeeper'
 /zk/bin/zkServer.sh start /zk/conf/zoo.cfg
 
 echo '[-] Staring Solr Cloud'
-/solr/bin/solr start -a "-DzkHost=localhost:2181" -force
+/solr/bin/solr start -p 80 -a "-DzkHost=localhost:2181" -force
 
 echo '[-] Creating data'
 /solr/bin/solr create -c Urahara -force
-/solr/bin/post -c Urahara /solr/example/exampledocs/*.xml
+/solr/bin/post -p 80 -c Urahara /solr/example/exampledocs/*.xml
 
 echo '[-] Dropping shell'
 echo ' -  HostName:   ' $(hostname)
