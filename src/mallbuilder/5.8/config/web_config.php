bb8f376c64c48eb499e02c2a280a5e3f7d5c8196
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-03-13 20:15:16 +0800

    Let us release that way

---
+++ src/mallbuilder/5.8/config/web_config.php
@@ -0,0 +1,6 @@
+<?php $config = array_merge($config, unserialize('a:20:{s:7:"company";s:11:"Mallbuilder";s:6:"weburl";s:14:"http://centos7";s:7:"baseurl";s:7:"centos7";s:4:"logo";s:0:"";s:8:"pay_name";s:9:"网付宝";s:6:"owntel";s:12:"021-64262959";s:5:"email";s:20:"zyfang821115@163.com";s:7:"regname";s:12:"register.php";s:9:"cacheTime";s:1:"0";s:5:"money";s:3:"￥";s:11:"date_format";s:8:"%Y-%m-%d";s:8:"language";s:2:"cn";s:4:"temp";s:7:"default";s:10:"domaincity";s:1:"0";s:11:"enable_gzip";s:1:"0";s:12:"enable_tranl";s:1:"0";s:14:"openstatistics";s:1:"0";s:9:"copyright";s:69:"MallBuilder版权所有,正版购买地址http://www.mall-builder.com";s:9:"closetype";s:1:"0";s:8:"closecon";s:0:"";}'));
+
+$config['weburl']  = 'http://' . $_SERVER['HTTP_HOST'];
+$config['baseurl'] = $_SERVER['HTTP_HOST'];
+
+?>
