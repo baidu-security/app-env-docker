3b61e7a0f33b17cf373a736b8c9f18a55cc64a2a
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-03-20 15:54:06 +0800

    Let us release that way

--- src/openrasp-buildenv/java6/build.sh
+++ src/openrasp-buildenv/java6/build.sh
@@ -12,7 +12,7 @@ else
 	git clone https://github.com/baidu/openrasp.git /tmp/openrasp
 fi
 
-time bash /tmp/openrasp/build.sh
+time bash /tmp/openrasp/build-java.sh
 
 mkdir -p /tmp/openrasp-release/java
 cp /tmp/openrasp/rasp-java.* /tmp/openrasp-release/java/
