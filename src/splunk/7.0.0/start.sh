6337bf59ae83c7c63a4a2302f34d903bd4e26b4e
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-04-01 10:22:49 +0800

    Let us release that way

--- src/splunk/7.0.0/start.sh
+++ src/splunk/7.0.0/start.sh
@@ -4,14 +4,15 @@ export JAVA_HOME=/jdk/
 
 echo '[-] Starting Splunk server'
 /splunk/bin/splunk start --answer-yes --accept-license --no-prompt
-
 echo
+
 echo '[-] Installing OpenRASP extension'
 /splunk/bin/splunk install app /tmp/splunk-app-1.0.zip -auth admin:changeme
-
 echo
+
 echo '[-] Adding TCP 5140 listener for OpenRASP'
 /splunk/bin/splunk add tcp 5140 -auth admin:changeme
+echo
 
 echo '[-] Dropping shell'
 echo ' -  HostName:   ' $(hostname)
