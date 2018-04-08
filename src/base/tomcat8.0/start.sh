921209379a4e84b4906154bdfc7e8747acc4098c
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-04-08 19:33:52 +0800

    Let us release that way

---
+++ src/base/tomcat8.0/start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export JAVA_HOME=/jdk/
+
+echo '[-] Starting Tomcat 7.0'
+bash /tomcat/bin/startup.sh
+
+echo '[-] Starting MariaDB'
+nohup mysqld_safe --datadir=/var/lib/mysql &>/dev/null &
+
+echo '[-] Waiting for MySQL to start ...'
+while true
+do
+	mysql -uroot -e 'select 1' &>/dev/null && break
+done
+
+echo '[-] Waiting for Tomcat to start ...'
+while true
+do
+	curl -I 127.0.0.1 2>/dev/null && break
+	sleep 1
+done
+
+echo '[-] Dropping shell'
+echo ' -  HostName:   ' $(hostname)
+echo ' -  IP address: ' $(ifconfig eth0 | awk '/inet / {print $2}')
+echo
+
+cd /tomcat/
+/bin/bash
+
