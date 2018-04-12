6fe64c701992ce65a9ad5b55e8431109c078c479
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-04-13 00:30:33 +0800

    Let us release that way

--- src/base/php5.4/start.sh
+++ src/base/php5.4/start.sh
@@ -7,7 +7,9 @@ echo '[-] Starting MySQL'
 /etc/init.d/mysql.sh start
 
 echo '[-] Accessing 127.0.0.1 for the first time'
-curl 127.0.0.1 &>/dev/null
+# 奇怪的BUG，第一次必然卡住
+curl 127.0.0.1 -m 1 &>/dev/null
+curl -I 127.0.0.1
 
 echo '[-] Dropping shell'
 echo ' -  HostName:   ' $(hostname)
