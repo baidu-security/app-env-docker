974f7f175bb0accd9ef6fdccbfe1fd8b2fb7a809
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-04-15 12:42:04 +0800

    php7.X

---
+++ src/base/php7.0/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+/etc/init.d/httpd.sh start
+/etc/init.d/mysql.sh start
+
+echo '[-] Accessing 127.0.0.1 for the first time'
+curl 127.0.0.1 &>/dev/null
+
+exec /etc/init.d/shell.sh /var/www/html
