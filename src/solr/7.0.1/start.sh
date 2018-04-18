5a5738dbcdba86f3d64fbeacca95ac150e488d64
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-04-18 19:53:44 +0800

    Let us release that way

--- src/solr/7.0.1/start.sh
+++ src/solr/7.0.1/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export JAVA_HOME=/jdk/
-export PATH=$PATH:/jdk/bin/
+export JAVA_HOME=/jdk
+export PATH=$PATH:$JAVA_HOME/bin
 
 echo '[-] Starting ZooKeeper'
 /zk/bin/zkServer.sh start /zk/conf/zoo.cfg
@@ -9,8 +9,4 @@ echo '[-] Starting ZooKeeper'
 echo '[-] Staring Solr Cloud'
 /solr/bin/solr start -p 80 -a "-DzkHost=localhost:2181" -force
 
-echo '[-] Creating data'
-/solr/bin/solr create -c Urahara -force
-/solr/bin/post -p 80 -c Urahara /solr/example/exampledocs/*.xml
-
-exec /etc/init.d/shell.sh /
+exec /etc/init.d/shell.sh /root/
