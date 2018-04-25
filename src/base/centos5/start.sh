279b49c28c5c098bd4d62c3a7963fa1a5d4c9c48
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-04-25 15:24:15 +0800

    解决缓存问题

--- src/base/centos5/start.sh
+++ src/base/centos5/start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo '[-] Extracting libs'
+for x in /download/*.tar.bz2
+do
+	echo ' -  '$x
+	tar -xf "$x" -C /
+done
+rm -rf /download/
+
 echo '[-] Dropping shell'
 echo ' -  HostName:   ' $(hostname)
 echo ' -  IP address: ' $(ifconfig eth0 | awk '/inet / {print $2}' | sed 's/addr://')
