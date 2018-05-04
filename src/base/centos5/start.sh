e659d04d68a415475c4ee6e30baba028dd5ac6f8
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-05-04 13:44:08 +0800

    Let us release that way

--- src/base/centos5/start.sh
+++ src/base/centos5/start.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-echo '[-] Extracting libs'
-for x in /download/*.tar.bz2
+echo '[-] Installing libs'
+for x in libs64.tar.bz2 libs32.tar.bz2 python2.7.tar.bz2 
 do
-	echo ' -  '$x
-	tar -xf "$x" -C /
+	curl https://packages.baidu.com/app/centos5/$x -o $x
+	tar -xf $x -C /
+	rm -f $x
 done
-rm -rf /download/
 
 echo '[-] Dropping shell'
 echo ' -  HostName:   ' $(hostname)
 echo ' -  IP address: ' $(ifconfig eth0 | awk '/inet / {print $2}' | sed 's/addr://')
 echo
 
-cd /root/scripts
 exec /bin/bash
