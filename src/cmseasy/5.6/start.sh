76709b593e82e3300aff7ce7aabf78acda7d8ed8
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-05-21 10:51:26 +0800

    Let us release that way

--- src/cmseasy/5.6/start.sh
+++ src/cmseasy/5.6/start.sh
@@ -3,10 +3,4 @@
 /etc/init.d/httpd.sh start
 /etc/init.d/mysql.sh start
 
-echo '[-] Accessing 127.0.0.1 for the first time'
-curl 127.0.0.1 &>/dev/null
-
-echo '[-] Setting docker ip in web config file'
-bash -c "/root/change-config.sh"
 exec /etc/init.d/shell.sh /var/www/html
-
