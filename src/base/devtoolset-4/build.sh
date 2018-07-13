c0c9a3e511911d361595ac241312889e12d6c098
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-07-13 18:04:56 +0800

    Let us release that way

--- src/base/devtoolset-4/build.sh
+++ src/base/devtoolset-4/build.sh
@@ -3,6 +3,7 @@
 set -ex
 
 use_antlrcpp=0
+php_version=$(echo "<?php echo phpversion(); ?>" | php)
 extra_config_opt=
 dest=/tmp/openrasp
 
@@ -13,15 +14,21 @@ else
 	git clone https://github.com/baidu/openrasp.git "$dest"
 fi
 
-# C++ 版本的 ANTLR，性能是 JS 版本的100倍
-if [[ $use_antlrcpp ]]; then
-	wget https://packages.baidu.com/app/openrasp/libantlr4-linux.tar.gz -O /tmp/libantlr4.tar.gz
-	tar -xf /tmp/libantlr4.tar.gz -C /tmp
-	export extra_config_opt="--with-antlr4=/tmp/libantlr4"
-fi
+# 可选: 使用 C++ 版本的 ANTLR；C++ 没有JIT快
+# 
+# if [[ $use_antlrcpp ]]; then
+# 	wget https://packages.baidu.com/app/openrasp/libantlr4-linux.tar.gz -O /tmp/libantlr4.tar.gz
+# 	tar -xf /tmp/libantlr4.tar.gz -C /tmp
+# 	export extra_config_opt="--with-antlr4=/tmp/libantlr4"
+# fi
 
 source /opt/rh/devtoolset-4/enable
-time bash "$dest"/build-php.sh
+
+if [[ $php_version =~ ^7 ]]; then
+	time bash "$dest"/build-php5.sh
+else
+	time bash "$dest"/build-php7.sh
+fi
 
 release=/tmp/rasp-php-$(date +%Y-%m-%d)
 for x in /tmp/openrasp/rasp-php-*/*
