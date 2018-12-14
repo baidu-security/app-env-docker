657e30273a5dd26d71d4fdbb39eed35200f7e9f9
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-12-14 12:50:37 +0800

    使用二进制的beego

--- src/openrasp-buildenv/panel/build-panel.sh
+++ src/openrasp-buildenv/panel/build-panel.sh
@@ -8,9 +8,13 @@ if ! [[ -d "$dest" ]]; then
 fi
 
 cd "$dest"
+tar -xf /root/bee-bin.tar -C cloud
+
 # rm -rf /tmp/openrasp/rasp-vue/node_modules/
 # ln -sf /root/node_modules/ /tmp/openrasp/rasp-vue/
 
 . /opt/rh/devtoolset-3/enable
 npm config set registry https://registry.npm.taobao.org
+
+export NO_BEE_INSTALL=1
 bash /tmp/openrasp/build-cloud.sh
