6b370e71a8461a6582d38a1bf7e9b412106fe777
Aaron Lewis <the.warl0ck.1989@gmail.com>
2019-04-17 21:01:27 +0800

    v1.1 之后使用 clang 替换 gcc

---
+++ src/base/clang-3.4/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -ex
+
+use_antlrcpp=0
+php_version=$(echo "<?php echo phpversion(); ?>" | php)
+extra_config_opt=
+dest=/tmp/openrasp
+
+if ! [[ -d "$dest" ]]; then
+	git clone https://github.com/baidu/openrasp.git "$dest"
+fi
+
+# 处理 #if defined(HAVE_BUNDLED_PCRE) || !defined(PHP_VERSION)
+for file in /usr/include/php/main/php_compat.h 
+do
+	if [[ -f "$file" ]]; then
+		sed -i 's~.*defined.*HAVE_BUNDLED_PCRE.*~#ifdef WHATEVER_YOU_LIKE~' "$file"
+	fi
+done
+
+cd "$dest"
+source /opt/rh/devtoolset-3/enable
+
+if [[ $php_version =~ ^7 ]]; then
+	time bash "$dest"/build-php7.sh
+else
+	time bash "$dest"/build-php5.sh
+fi
+
+release=/tmp/rasp-php-$(date +%Y-%m-%d)
+for x in /tmp/openrasp/rasp-php-*/*
+do
+	cp -R "$x" "$release"/
+done
