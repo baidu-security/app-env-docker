29cc6913edf03494330ab2de1177d3420f97e3e3
Aaron Lewis <the.warl0ck.1989@gmail.com>
2019-04-18 11:54:31 +0800

    修正脚本

--- src/base/devtoolset-3/openrasp-v8.sh
+++ src/base/devtoolset-3/openrasp-v8.sh
@@ -3,7 +3,7 @@ set -x
 
 cd /tmp/
 curl -L https://github.com/baidu-security/openrasp-v8/archive/master.zip -o openrasp-v8.zip
-unzip openrasp-v8.zip
+unzip -o -qq openrasp-v8.zip
 rm -f openrasp-v8.zip
 
 mkdir openrasp-v8-master/php/build
