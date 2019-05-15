71fed900f98d698e231ecfd1da4c6fbc5aaa6405
Aaron Lewis <the.warl0ck.1989@gmail.com>
2019-05-15 11:47:22 +0800

    slave test

---
+++ src/mysql/5.7.17/start-slave.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+master_ip=$1
+
+if [[ -z "$master_ip" ]]; then
+    echo $0 master_ip
+    exit
+fi
+
+export PATH=$PATH:/usr/local/mysql/bin
+
+/usr/local/mysql/support-files/mysql.server stop
+rm -f /data/mysql/auto.cnf
+cp /etc/my-slave.cnf /etc/my.cnf
+/usr/local/mysql/support-files/mysql.server start
+
+cat >> /tmp/slave.sql << EOF
+CHANGE MASTER TO MASTER_HOST='$master_ip',MASTER_USER='slave_user', MASTER_PASSWORD='slave_pass', MASTER_LOG_FILE='master.000002', MASTER_LOG_POS=1460;
+START SLAVE;
+EOF
+
+mysql -uroot < /tmp/slave.sql
+mysql -uroot -e 'SHOW SLAVE STATUS\G'
--- src/mysql/5.7.17/start.sh
+++ src/mysql/5.7.17/start.sh
@@ -2,7 +2,23 @@
 
 export PATH=$PATH:/usr/local/mysql/bin
 
+echo '[-] Starting MySQL server'
 /usr/local/mysql/support-files/mysql.server start
 
-mysql -uroot -e "CREATE USER 'root'@'%' IDENTIFIED BY ''; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
+cat > /tmp/start.sql << EOF
+CREATE USER 'root'@'%' IDENTIFIED BY ''; 
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+GRANT REPLICATION SLAVE ON *.* TO 'slave_user'@'%' IDENTIFIED BY 'slave_pass';
+
+CREATE DATABASE IF NOT EXISTS test;
+CREATE TABLE test.user (id INT, name TEXT);
+INSERT INTO test.user VALUES (1, "abc");
+EOF
+
+mysql -uroot < /tmp/start.sql
+rm -f /tmp/start.sql
+
+mysql -uroot -e 'show master status'
+
 exec /etc/init.d/shell.sh /root/
+
