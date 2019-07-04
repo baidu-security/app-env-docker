b8d93e0393c1f9d2d6b5bc530e3f5c4b9214e999
Aaron Lewis <the.warl0ck.1989@gmail.com>
2019-07-04 19:35:36 +0800

    调整编译脚本

--- src/openrasp-buildenv/php-centos6-php-ts/build-ts-rasp.sh
+++ src/openrasp-buildenv/php-centos6-php-ts/build-ts-rasp.sh
@@ -2,5 +2,6 @@
 
 for ver in 5.{3..6} 7.{0..3}
 do
-	PATH=/usr/local/php-${ver}/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/jdk/bin /root/build.sh
+	export PATH=/usr/local/php-${ver}/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/jdk/bin 
+	/root/build.sh
 done
