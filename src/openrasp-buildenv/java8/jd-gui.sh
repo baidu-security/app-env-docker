da637eba458a88dd078f4ff2bc7d7d677d25e2ef
Aaron Lewis <the.warl0ck.1989@gmail.com>
2019-09-07 10:29:10 +0800

    jd-gui compile

---
+++ src/openrasp-buildenv/java8/jd-gui.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf master.zip jd-gui-master
+
+wget https://github.com/java-decompiler/jd-gui/archive/master.zip -O master.zip
+unzip -qq master.zip
+cd jd-gui-master
+./gradlew
+
