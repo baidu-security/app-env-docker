cd585810e0e868415e524cb75fa96d6b4667d8e7
Aaron Lewis <the.warl0ck.1989@gmail.com>
2019-09-09 14:55:54 +0800

    fix sudo

--- src/openldap/2.4.44/start.sh
+++ src/openldap/2.4.44/start.sh
@@ -5,10 +5,12 @@ useradd user1
 
 echo '[-] Staring slapd'
 slapd -h 'ldapi:/// ldap:/// ldaps:///'
+# slapd -h 'ldapi:/// ldap:/// ldaps:///' -f /etc/openldap/slapd.conf
 
 echo '[-] Init LDAP server'
-for x in chrootpw.ldif /etc/openldap/schema/{sudo.schema,openssh.schema,cosine.ldif,nis.ldif,inetorgperson.ldif} openssh-lpk.ldif sudo.ldif
+for x in chrootpw.ldif /etc/openldap/schema/{sudo.schema,openssh.schema,cosine.ldif,nis.ldif,inetorgperson.ldif} openssh-lpk.ldif sudo.ldif index.ldif
 do
+	echo "-> $x"
     ldapadd -Y EXTERNAL -H ldapi:/// -f "$x"
 done
 
@@ -16,7 +18,7 @@ ldapmodify -Y EXTERNAL -H ldapi:/// -f chdomain.ldif
 ldapadd -x -w 123456 -D cn=Manager,dc=srv,dc=world -f basedomain.ldif
 
 echo '[-] Staring SSSD'
-/usr/sbin/sssd
+/usr/sbin/sssd -i -d10 &> /tmp/sssd.log &
 
 echo '[-] Starting sshd'
 ssh-keygen -N "" -f /root/.ssh/id_rsa
@@ -25,18 +27,49 @@ echo AuthorizedKeysCommandUser nobody >> /etc/ssh/sshd_config
 /usr/sbin/sshd-keygen
 /usr/sbin/sshd
 
+mv /etc/pam.d/{system-auth-1,system-auth}
+mv /etc/pam.d/{password-auth-1,password-auth}
+
+echo "Debug sudo /var/log/sudo_debug.log all@debug" >> /etc/sudo.conf
+echo "Debug sudoers.so /var/log/sudo_debug.log all@debug" >> /etc/sudo.conf
+
 echo '[-] Adding test:test & search:test user'
 cat > ssh-user.ldif << EOF
-cn: SUDO users
-dn: cn=role1,ou=SUDOers,ou=People,dc=srv,dc=world
-objectClass: sudoRole
+# sudo config
+dn: cn=defaults,ou=SUDOers,ou=People,dc=srv,dc=world
 objectClass: top
-sudoCommand: ALL
-sudoHost: ALL
-sudoUser: test
-sudoOption: badpass_message=SUDO LDAP does not work, WTF
+objectClass: sudoRole
+cn: defaults
+description: Default sudoOption's go here
+sudoOption: secure_path = /sbin:/bin:/usr/sbin:/usr/bin
 
-dn: uid=test,ou=People,dc=srv,dc=world
+dn: cn=test,ou=SUDOers,ou=People,dc=srv,dc=world
+objectClass: top
+objectClass: sudoRole
+cn: test
+sudoUser: test
+sudoHost: ALL
+sudoRunAsUser: ALL
+sudoCommand: ALL
+
+dn: cn=test2,ou=SUDOers,ou=People,dc=srv,dc=world
+objectClass: top
+objectClass: sudoRole
+cn: test2
+sudoUser: test2
+sudoHost: ALL
+sudoRunAsUser: ALL
+sudoCommand: ALL
+sudoOption: !authenticate
+
+# groups
+dn: cn=testgroup,ou=People,dc=srv,dc=world
+objectClass: top
+objectClass: posixGroup
+gidNumber: 2000
+
+# userlist
+dn: cn=test,ou=People,dc=srv,dc=world
 cn: Test Account
 uid: test
 uidNumber: 2000
@@ -51,8 +84,24 @@ objectClass: ldapPublicKey
 userPassword: {SSHA}5UtMxV1rgztbZeSpI0TiGgw20sSb7muq
 sshPublicKey: $(cat /root/.ssh/id_rsa.pub)
 
+dn: cn=test2,ou=People,dc=srv,dc=world
+cn: Test Account 2
+uid: test2
+uidNumber: 2001
+gidNumber: 2000
+homeDirectory: /home/test2
+loginShell: /bin/bash
+objectClass: account
+objectClass: posixAccount
+objectClass: top
+objectClass: shadowAccount
+objectClass: ldapPublicKey
+userPassword: {SSHA}5UtMxV1rgztbZeSpI0TiGgw20sSb7muq
+sshPublicKey: $(cat /root/.ssh/id_rsa.pub)
+
+# search
 dn: cn=search,ou=People,dc=srv,dc=world
-cn: search
+cn: Search Account
 gidNumber: 2001
 homeDirectory: /home/clientsearch
 loginShell: /sbin/nologin
