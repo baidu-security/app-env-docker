f44e15f4b6ea8dffe51a2945c02b2920a6ab17bb
Aaron Lewis <the.warl0ck.1989@gmail.com>
2019-09-24 19:02:34 +0800

    去掉日期

--- src/openrasp-buildenv/iast/build.sh
+++ src/openrasp-buildenv/iast/build.sh
@@ -13,4 +13,5 @@ cd /tmp/openrasp-iast/
 make build
 
 DATE=$(TZ=UTC-8 date -d @`git log -n 1 --pretty="%cd" --date=raw | cut -d " " -f 1` +'%Y%m%d%H%M%S')
-mv /tmp/openrasp-iast/openrasp_iast/dist/openrasp-iast /tmp/openrasp-iast/openrasp-iast-${DATE}
+#mv /tmp/openrasp-iast/openrasp_iast/dist/openrasp-iast /tmp/openrasp-iast/openrasp-iast-${DATE}
+mv /tmp/openrasp-iast/openrasp_iast/dist/openrasp-iast /tmp/openrasp-iast/openrasp-iast-latest
