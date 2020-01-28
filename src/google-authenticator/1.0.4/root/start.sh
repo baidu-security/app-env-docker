7188f33dc5f264b4bb9ab298ae577e663e762598
Aaron Lewis <the.warl0ck.1989@gmail.com>
2020-01-28 21:27:02 +0800

    调整目录结构

--- src/google-authenticator/1.0.4/root/start.sh
+++ src/google-authenticator/1.0.4/root/start.sh
@@ -3,7 +3,9 @@
 /usr/sbin/sshd-keygen
 /usr/sbin/sshd
 
+echo
 echo '[-] Login codes for testing purpose'
 oathtool -d 6 -b MKOCAYGSNJEGOVRYU6K3MONYD4 -c 1 --hotp -w 3
+echo
 
 /etc/init.d/shell.sh /root
