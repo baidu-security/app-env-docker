e0a52ce29c0e27ef01b007500d6e266ab2ecc663
Aaron Lewis <the.warl0ck.1989@gmail.com>
2020-01-28 21:32:10 +0800

    增加bind version屏蔽

--- src/bind/9.9.4/root/start.sh
+++ src/bind/9.9.4/root/start.sh
@@ -7,6 +7,11 @@ chmod 777 -R /run/named/ /var/named/
 
 echo '[-] To launch zone transfer'
 echo ' -  dig @127.0.0.1 example.com AXFR'
+echo ' -  dig @127.0.0.1 example.com XFR'
+echo
+
+echo '[-] To retrieve NAMED version'
+echo ' -  dig @127.0.0.1 -c CH -t txt version.bind'
 
 dig @127.0.0.1 example.com AXFR
 
