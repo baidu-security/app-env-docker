e865f7e0b98dd16317034b7c2f630a42db7511ed
Aaron Lewis <the.warl0ck.1989@gmail.com>
2020-01-28 21:40:46 +0800

    增加ns解析

--- src/bind/9.9.4/root/start.sh
+++ src/bind/9.9.4/root/start.sh
@@ -5,15 +5,10 @@ mkdir -p /var/named/dynamic
 chmod 777 -R /run/named/ /var/named/
 /usr/sbin/named
 
-echo '[-] To launch zone transfer'
-echo ' -  dig @127.0.0.1 example.com AXFR'
-echo ' -  dig @127.0.0.1 example.com XFR'
-echo
-
-echo '[-] To retrieve NAMED version'
-echo ' -  dig @127.0.0.1 -c CH -t txt version.bind'
-
-dig @127.0.0.1 example.com AXFR
+echo '[-] Commands for testing purpose'
+echo '    dig @127.0.0.1 example.com AXFR'
+echo '    dig @127.0.0.1 -c CH -t txt version.bind'
+echo '    dig @127.0.0.1 -t ANY example.com'
 
 exec /etc/init.d/shell.sh /
 
