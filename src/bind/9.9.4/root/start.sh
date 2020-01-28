58e75e8b6568709d29b9287d45e1fadcb1da287a
Aaron Lewis <the.warl0ck.1989@gmail.com>
2020-01-28 21:42:40 +0800

    增加换行

--- src/bind/9.9.4/root/start.sh
+++ src/bind/9.9.4/root/start.sh
@@ -5,10 +5,12 @@ mkdir -p /var/named/dynamic
 chmod 777 -R /run/named/ /var/named/
 /usr/sbin/named
 
+echo
 echo '[-] Commands for testing purpose'
 echo '    dig @127.0.0.1 example.com AXFR'
 echo '    dig @127.0.0.1 -c CH -t txt version.bind'
 echo '    dig @127.0.0.1 -t ANY example.com'
+echo
 
 exec /etc/init.d/shell.sh /
 
