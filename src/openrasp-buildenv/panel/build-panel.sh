d9bdd149275571c1f46b98e73bedfc082c6e4f1a
Aaron Lewis <the.warl0ck.1989@gmail.com>
2020-02-07 08:50:51 +0800

    go mod 缓存

--- src/openrasp-buildenv/panel/build-panel.sh
+++ src/openrasp-buildenv/panel/build-panel.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 set -ex
-dest=/tmp/openrasp
+. /opt/rh/devtoolset-3/enable
 
+dest=/tmp/openrasp
 if ! [[ -d "$dest" ]]; then
-	git clone -b 1.0rc1 https://github.com/baidu/openrasp.git "$dest"
+	git clone https://github.com/baidu/openrasp.git "$dest"
 fi
 
 cd "$dest"
-tar -xf /root/bee-bin.tar -C cloud
 
+tar -xf /root/bee-bin.tar -C cloud
+mkdir -p /root/go
+curl https://packages.baidu.com/app/openrasp/cloud-files/pkg.tar.gz -o - | tar -xzf - -C /root/go
+
+npm config set registry https://registry.npm.taobao.org
 # rm -rf /tmp/openrasp/rasp-vue/node_modules/
 # ln -sf /root/node_modules/ /tmp/openrasp/rasp-vue/
 
-. /opt/rh/devtoolset-3/enable
-npm config set registry https://registry.npm.taobao.org
-
 export NO_BEE_INSTALL=1
+export NO_GOMOD_DOWNLOAD=1
 bash /tmp/openrasp/build-cloud.sh
