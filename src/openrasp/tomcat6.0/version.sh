b78cd10a6272f67161f80088c52b677102085133
Aaron Lewis <the.warl0ck.1989@gmail.com>
2020-02-07 09:01:54 +0800

    支持切换rasp-java版本

---
+++ src/openrasp/tomcat6.0/version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+version=${1:-latest}
+shift
+
+echo '[-] Download and install rasp-java'
+rm -rf /tmp/rasp-java*
+curl "https://packages.baidu.com/app/openrasp/release/${version}/rasp-java.tar.gz" -o - | tar -xzf - -C /tmp
+
+cd /tmp/rasp-*
+/jdk/bin/java -jar /tmp/rasp-*/RaspInstall.jar -install /tomcat/ "$@"
+
+curl https://packages.baidu.com/app/openrasp/999-event-logger.js -o /tomcat/rasp/plugins/999-event-logger.js
+
+if pidof java 1>/dev/null; then
+	/etc/init.d/tomcat.sh stop
+	/etc/init.d/tomcat.sh start
+fi
+
