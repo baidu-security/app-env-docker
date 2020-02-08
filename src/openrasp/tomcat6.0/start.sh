5930f519731a322b523205b49fa50dd2af0c8e7d
Aaron Lewis <the.warl0ck.1989@gmail.com>
2020-02-08 09:04:40 +0800

    tomcat 6 增加数据库初始化

--- src/openrasp/tomcat6.0/start.sh
+++ src/openrasp/tomcat6.0/start.sh
@@ -6,4 +6,18 @@ export JAVA_HOME=/jdk/
 /etc/init.d/tomcat.sh start
 /etc/init.d/mysql.sh start
 
+echo '[-] Adding MySQL user test@localhost'
+cat > /tmp/test.sql << EOF
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE test;
+grant all privileges on test.* to 'test'@'%' identified by 'test';
+grant all privileges on test.* to 'test'@'localhost' identified by 'test';
+CREATE TABLE test.vuln (id INT, name text);
+INSERT INTO test.vuln values (0, "openrasp");
+INSERT INTO test.vuln values (1, "rocks");
+EOF
+
+mysql -uroot < /tmp/test.sql
+rm -f /tmp/test.sql
+
 exec /etc/init.d/shell.sh /tomcat/
