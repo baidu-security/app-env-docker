d2f44f3d709aef050478b8986e55bff00d258bc9
Aaron Lewis <commit@github.com>
2021-02-15 08:20:55 +0800

    centos6改为vault仓库

--- src/openrasp-buildenv/all-in-one/root/build-php-binary.sh
+++ src/openrasp-buildenv/all-in-one/root/build-php-binary.sh
@@ -21,30 +21,34 @@ build_php()
 	rm -rf php-*
 }
 
-build_php https://packages.baidu.com/app/php-5.3/php-5.3.27.tar.bz2 /usr/local/php/5.3
-build_php https://packages.baidu.com/app/php-5.3/php-5.3.27.tar.bz2 /usr/local/php/5.3-ts --enable-maintainer-zts
+declare -A php_versions
+php_versions[5.3]=https://packages.baidu.com/app/php-5.3/php-5.3.27.tar.bz2
+php_versions[5.4]=https://packages.baidu.com/app/php-5.4/php-5.4.45.tar.bz2
+php_versions[5.5]=https://packages.baidu.com/app/php-5.5/php-5.5.38.tar.bz2
+php_versions[5.6]=https://packages.baidu.com/app/php-5.6/php-5.6.40.tar.bz2
+php_versions[7.0]=https://packages.baidu.com/app/php-7.0/php-7.0.33.tar.bz2
+php_versions[7.1]=https://packages.baidu.com/app/php-7.1/php-7.1.9.tar.bz2
+php_versions[7.2]=https://packages.baidu.com/app/php-7.2/php-7.2.14.tar.bz2
+php_versions[7.3]=https://packages.baidu.com/app/php-7.3/php-7.3.17.tar.bz2
+php_versions[7.4]=https://packages.baidu.com/app/php-7.4/php-7.4.15.tar.bz2
 
-build_php https://packages.baidu.com/app/php-5.4/php-5.4.45.tar.bz2 /usr/local/php/5.4
-build_php https://packages.baidu.com/app/php-5.4/php-5.4.45.tar.bz2 /usr/local/php/5.4-ts --enable-maintainer-zts
+for ver in ${!php_versions[@]}
+do
+	url=${php_versions[$ver]}
 
-build_php https://packages.baidu.com/app/php-5.5/php-5.5.38.tar.bz2 /usr/local/php/5.5
-build_php https://packages.baidu.com/app/php-5.5/php-5.5.38.tar.bz2 /usr/local/php/5.5-ts --enable-maintainer-zts
+	if [[ ! -f /usr/local/php/$ver/bin/php ]]; then
+		build_php $url /usr/local/php/$ver
+	else
+		echo Skipped existing PHP version $ver
+	fi
 
-build_php https://packages.baidu.com/app/php-5.6/php-5.6.40.tar.bz2 /usr/local/php/5.6
-build_php https://packages.baidu.com/app/php-5.6/php-5.6.40.tar.bz2 /usr/local/php/5.6-ts --enable-maintainer-zts
+	if [[ ! -f /usr/local/php/$ver-ts/bin/php ]]; then
+		build_php $url /usr/local/php-$ver-ts --enable-maintainer-zts
+	else
+		echo Skipped existing PHP-TS version $ver
+	fi
+done
 
-build_php https://packages.baidu.com/app/php-7.0/php-7.0.33.tar.bz2 /usr/local/php/7.0
-build_php https://packages.baidu.com/app/php-7.0/php-7.0.33.tar.bz2 /usr/local/php/7.0-ts --enable-maintainer-zts
 
-build_php https://packages.baidu.com/app/php-7.1/php-7.1.9.tar.bz2 /usr/local/php/7.1
-build_php https://packages.baidu.com/app/php-7.1/php-7.1.9.tar.bz2 /usr/local/php/7.1-ts --enable-maintainer-zts
 
-build_php https://packages.baidu.com/app/php-7.2/php-7.2.14.tar.bz2 /usr/local/php/7.2
-build_php https://packages.baidu.com/app/php-7.2/php-7.2.14.tar.bz2 /usr/local/php/7.2-ts --enable-maintainer-zts
-
-build_php https://packages.baidu.com/app/php-7.3/php-7.3.17.tar.bz2 /usr/local/php/7.3
-build_php https://packages.baidu.com/app/php-7.3/php-7.3.17.tar.bz2 /usr/local/php/7.3-ts --enable-maintainer-zts
-
-build_php https://packages.baidu.com/app/php-7.4/php-7.4.15.tar.bz2 /usr/local/php/7.4
-build_php https://packages.baidu.com/app/php-7.4/php-7.4.15.tar.bz2 /usr/local/php/7.4-ts --enable-maintainer-zts
 
