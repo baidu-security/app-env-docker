7f277658799f546253e82651282d6f86a11d84ae
Aaron Lewis <commit@github.com>
2021-02-15 15:00:23 +0800

    修正脚本

--- src/openrasp-buildenv/all-in-one/root/build.sh
+++ src/openrasp-buildenv/all-in-one/root/build.sh
@@ -2,19 +2,25 @@
 # 在编译机，github 经常无法 pull，需要走代理
 # 所以 docker 内部不再尝试 pull，统一再外部更新
 
-set -ex
-
-use_antlrcpp=0
-php_version=$(php -r 'echo phpversion();')
-extra_config_opt=
 dest=/tmp/openrasp
 
-if ! [[ -d "$dest" ]]; then
-	git clone https://github.com/baidu/openrasp.git "$dest"
+if [[ -z "$php_version" ]]; then
+	echo PHP_VERSION must be set
+	exit 1
 fi
 
-cd "$dest"
+if [[ ! -f "$dest"/build-php7.sh ]] || [[ ! -f "$dest"/build-php5.sh ]]; then
+	echo Invalid openrasp source folder in $dest
+	exit 1
+fi
+
+if [[ ! -f /opt/rh/devtoolset-4/enable ]]; then
+	echo devtoolset-4 is required to build php extension
+	exit 1
+fi
+
 source /opt/rh/devtoolset-4/enable
+set -ex
 
 if [[ $php_version =~ ^7 ]]; then
 	time bash "$dest"/build-php7.sh
