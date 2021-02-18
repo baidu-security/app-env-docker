a2409f7cc28b012fbafc9afa4a9b937a500ccfd8
Aaron Lewis <commit@github.com>
2021-02-18 17:22:37 +0800

    删除多余脚本

--- src/openrasp-buildenv/all-in-one/root/build-ts-rasp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for ver in 5.{3..6} 7.{0..3}
-do
-	export PATH=/usr/local/php-${ver}/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/jdk/bin 
-	/root/build.sh
-done
