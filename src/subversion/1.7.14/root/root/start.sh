f70a910bfb188188e542697567f1eba1229a15df
commiter <commit@github.com>
2021-10-03 16:14:05 +0800

    修正svn

--- src/subversion/1.7.14/root/root/start.sh
+++ src/subversion/1.7.14/root/root/start.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
-echo '[-] Starting httpd daemon and svn module'
+echo '[-] Configure svn repo'
+rm -f /etc/httpd/conf.d/welcome.conf
+
+mkdir -p /svn
+svnadmin create /svn/repo1
+chown -R apache:apache /svn/repo1
+
+mkdir -p /etc/svn
+echo 123456 | htpasswd -icm /etc/svn/svn-auth admin
+echo 123456 | htpasswd -im /etc/svn/svn-auth user1
+echo 123456 | htpasswd -im /etc/svn/svn-auth user2
+chown root:apache /etc/svn/svn-auth
+chmod 640 /etc/svn/svn-auth
+
+cat > /svn/repo1/conf/authz << EOF
+[groups]
+admin=admin
+repo1_user1=user1
+repo1_user2=user2
+
+[/]
+@admin=rw
+
+[repo1:/]
+@repo1_user=rw
+@repo1_user2=r
+EOF
+
 /etc/init.d/httpd.sh start
 
 exec /etc/init.d/shell.sh /
