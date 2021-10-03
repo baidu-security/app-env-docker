b37137dc99d480a5f036a1656fdc5ffe9fb65bf0
commiter <commit@github.com>
2021-10-03 16:17:57 +0800

    Let us release that way

--- src/subversion/1.7.14/root/root/start.sh
+++ src/subversion/1.7.14/root/root/start.sh
@@ -3,18 +3,18 @@
 echo '[-] Configure svn repo'
 rm -f /etc/httpd/conf.d/welcome.conf
 
+# repo
 mkdir -p /svn
 svnadmin create /svn/repo1
 chown -R apache:apache /svn/repo1
 
+# auth
 mkdir -p /etc/svn
 echo 123456 | htpasswd -icm /etc/svn/svn-auth admin
 echo 123456 | htpasswd -im /etc/svn/svn-auth user1
 echo 123456 | htpasswd -im /etc/svn/svn-auth user2
-chown root:apache /etc/svn/svn-auth
-chmod 640 /etc/svn/svn-auth
 
-cat > /svn/repo1/conf/authz << EOF
+cat > /etc/svn/authz << EOF
 [groups]
 admin=admin
 repo1_user1=user1
@@ -24,10 +24,14 @@ repo1_user2=user2
 @admin=rw
 
 [repo1:/]
-@repo1_user=rw
+@repo1_user1=rw
 @repo1_user2=r
 EOF
 
+chown root:apache /etc/svn/svn-auth
+chmod 640 /etc/svn/*
+
+# start service
 /etc/init.d/httpd.sh start
 
 exec /etc/init.d/shell.sh /
