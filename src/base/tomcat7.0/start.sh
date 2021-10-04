de43d0be58c88cb538c6b64ce196551f3dcb9502
commiter <commit@github.com>
2021-10-04 16:42:27 +0800

    [tomcat] 增加mod_proxy测试

--- src/base/tomcat7.0/start.sh
+++ src/base/tomcat7.0/start.sh
@@ -2,5 +2,9 @@
 
 /etc/init.d/tomcat.sh start
 /etc/init.d/mysql.sh start
+apachectl -k start
+
+mkdir -p /tomcat/webapps/test
+echo 123 > /tomcat/webapps/test/index.jsp
 
 exec /etc/init.d/shell.sh /tomcat/
