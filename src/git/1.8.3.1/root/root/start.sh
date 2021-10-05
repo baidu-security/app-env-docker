142f27424f5a1b8fc0369630371b00a3938309d8
commiter <commit@github.com>
2021-10-05 18:32:34 +0800

    [git] 修正脚本

--- src/git/1.8.3.1/root/root/start.sh
+++ src/git/1.8.3.1/root/root/start.sh
@@ -3,26 +3,38 @@
 echo '[-] Configure svn repo'
 rm -f /etc/httpd/conf.d/welcome.conf
 
+# git
+mkdir -p /root
+git config --global user.email "commiter@github.com"
+git config --global user.name "Anonymous User"
+git config --global push.default simple
+
 # repo
 mkdir -p /var/www/html/git/test.git
 cd /var/www/html/git/test.git
 git --bare init
 git update-server-info
-chown -R www-data.www-data .
-chmod -R 755 .
+chmod -R 777 .
 
 # auth
-echo 123456 | htpasswd -icm /etc/git-htpasswd admin
 echo 123456 | htpasswd -im /etc/git-htpasswd user1
 echo 123456 | htpasswd -im /etc/git-htpasswd user2
 
 nohup fcgiwrap -s unix:/var/run/fcgiwrap.socket &
+sleep 1
 chmod 777 /var/run/fcgiwrap.socket
 
 # start service
 /etc/init.d/nginx.sh start
 
-echo '[-] Testing access'
-curl http://user1:123456@127.0.0.1/git/test.git/config; echo
+echo '[-] Testing commit'
+echo
+git clone http://user1:123456@127.0.0.1/test.git my-repo
+cd my-repo
+echo 123 > test
+git add test
+git commit -a -m test
+git push
+echo
 
-exec /etc/init.d/shell.sh /
+exec /etc/init.d/shell.sh /root
